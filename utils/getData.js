1d26d66afc2a055402ca7865bbc59354887fa228
Hendyley <hendylim74@gmail.com>
2026-09-20 23:07:42 +0800

    add for node 20/9

---
+++ utils/getData.js
@@ -0,0 +1,12 @@
+import path from 'node:path'
+import fs from 'node:fs/promises'
+
+export async function getData() {
+
+    try {
+        const pathJSON = path.join('data', 'data.json')
+    }
+    catch (err){
+        
+    }
+}
